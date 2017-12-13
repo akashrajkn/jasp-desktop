8e570e360b35b139b6efb9cce2a2438813b39108
akashrajkn <akashrajkn@gmail.com>
2017-12-13 19:55:52 +0100

    First Draft = multinomial

--- JASP-Desktop/widgets/boundtablewidget.cpp
+++ JASP-Desktop/widgets/boundtablewidget.cpp
@@ -25,15 +25,13 @@ BoundTableWidget::BoundTableWidget(QWidget *parent)
 {
 	_boundTo = NULL;
 
+
 	// connect(this, SIGNAL(cellChanged()), this, SLOT(callNotifyChanged()));
 }
 
 void BoundTableWidget::bindTo(Option *option)
 {
 	_boundTo = dynamic_cast<OptionsTable *>(option);
-	qDebug() << "-----------------------------------------";
-	qDebug() << "_boundTo";
-	qDebug() << "-----------------------------------------";
 
 	if (_boundTo != NULL) {
 		_groups = _boundTo->value();
@@ -52,58 +50,121 @@ void BoundTableWidget::callNotifyChanged()
 
 void BoundTableWidget::refresh()
 {
-	// beginResetModel();
-
 	int columnCount = this->columnCount();
 	int rowCount = this->rowCount();
 
 	qDebug() << "-----------------------";
-	qDebug() << QString::number(columnCount);
-	qDebug() << QString::number(rowCount);
 
+	_groups.clear();
+
+	// Options *newRow = static_cast<Options *>(_boundTo->rowTemplate()->clone());
+	std::vector<std::string> verticalHeaders;
+	OptionVariables *headers = static_cast<OptionVariables *>(_boundTo->rowTemplate()->clone()->get("factors"));
+
+	// Get verticalHeaders
 	for (int i = 0; i < rowCount; ++i) {
-		for (int j = 0; j < columnCount; ++j) {
-			QTableWidgetItem *a = this->item();
-			QString content = "None";
-			if (a != NULL) {
-				qDebug() << "Not None";
-				content = a->text();
+		QTableWidgetItem *verticalHeaderItem = this->horizontalHeaderItem(i);
+		QString header = "";
+		if (verticalHeaderItem != NULL) {
+			header = verticalHeaderItem->text();
+		}
+
+		verticalHeaders.push_back(fq(header));
+	}
+
+	for (int i = 0; i < columnCount; ++i) {
+		Options *newRow = static_cast<Options *>(_boundTo->rowTemplate()->clone());  // column is the row here
+		OptionString *factorName = static_cast<OptionString *>(newRow->get("name"));
+		QString header = "";
+		QTableWidgetItem *horizontalHeaderItem = this->horizontalHeaderItem(i);
+		if (horizontalHeaderItem != NULL) {
+			header = horizontalHeaderItem->text();
+		} else {
+			// TODO: Revisit this part
+			continue;
+		}
+		factorName->setValue(fq(header));
+
+		OptionVariables *option = static_cast<OptionVariables *>(newRow->get("factor_values"));
+		std::vector<std::string> factor_values;
+		std::vector<std::string> factors;
+
+		for (int j = 0; j < rowCount; ++j) {
+			QTableWidgetItem *rowItem = this->item(j, i);
+			QString content = "";
+
+			if (rowItem != NULL) {
+				content = rowItem->text();
 			}
+
+			factor_values.push_back(fq(content));
+
 			qDebug() << "row " << QString::number(i) << ", " << "column " << QString::number(j) << ":: " << content; // << this->item(i, j)->text();
 		}
+
+		option->setValue(factor_values);
+
+		_groups.push_back(newRow);
 	}
 
-	qDebug() << "-----------------------";
+// 	qDebug() << "------BoundTable constructor------";
+// 	Options *newRow = static_cast<Options *>(_boundTo->rowTemplate()->clone());
+// 	OptionString *factorName = static_cast<OptionString *>(newRow->get("name"));
+// 	factorName->setValue(fq("NewHypothesis"));
+//
+// 	OptionVariables *option = static_cast<OptionVariables *>(newRow->get("factors"));
+// 	std::vector<std::string> test;
+// 	test.push_back("alpha");
+// 	test.push_back("beta");
+// 	option->setValue(test);
+//
+// 	_groups.push_back(newRow);
+// 	_boundTo->setValue(_groups);
+// 	_groups.clear();
+// 	qDebug() << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$";
+	//
+	// qDebug() << "-----------------------";
 
-	size_t i;
-
-	// hypothesis
-	OptionString *nameTemplateOption = static_cast<OptionString *>(_boundTo->rowTemplate()->get("name"));
-	QString nameTemplate = tq(nameTemplateOption->value());
-
-    for (i = 0; i < _groups.size(); i++)
-	{
-		Options *group = _groups.at(i);
-		OptionString *nameOption = static_cast<OptionString *>(group->get("name"));
-		std::string oldName = nameOption->value();
-
-		OptionVariables *factorsOption = static_cast<OptionVariables *>(group->get("factors"));
-		OptionVariables *valuesOptions = static_cast<OptionVariables *>(group->get("factor_values"));
-		// _rows.append(Row(tq(oldName), false, i));
-		qDebug() << "oldName = " << tq(oldName);
-
-		std::vector<std::string> factors = factorsOption->variables();
-
-		size_t j;
-		for (j = 0; j < factors.size(); j++)
-			qDebug() << QString::number(j) << ": " << tq(factors.at(j));
-			// _rows.append(Row(tq(factors.at(j)), false, i, j));
-
-		// _rows.append(Row(QString("Level %1").arg(j + 1), true, i, j));
-	}
-
-	QString name = nameTemplate.arg(i + 1);
+	// size_t i;
+	//
+	// // hypothesis
+	// OptionString *nameTemplateOption = static_cast<OptionString *>(_boundTo->rowTemplate()->get("name"));
+	// QString nameTemplate = tq(nameTemplateOption->value());
+	//
+	// for (i = 0; i < _groups.size(); i++)
+	// {
+	// 	Options *group = _groups.at(i);
+	// 	OptionString *nameOption = static_cast<OptionString *>(group->get("name"));
+	// 	std::string oldName = nameOption->value();
+	//
+	// 	OptionVariables *factorsOption = static_cast<OptionVariables *>(group->get("factors"));
+	// 	OptionVariables *valuesOptions = static_cast<OptionVariables *>(group->get("factor_values"));
+	// 	// _rows.append(Row(tq(oldName), false, i));
+	// 	qDebug() << "oldName = " << tq(oldName);
+	//
+	// 	std::vector<std::string> factors = factorsOption->variables();
+	//
+	// 	size_t j;
+	// 	for (j = 0; j < factors.size(); j++)
+	// 		qDebug() << QString::number(j) << ": " << tq(factors.at(j));
+	// 		// _rows.append(Row(tq(factors.at(j)), false, i, j));
+	//
+	// 	// _rows.append(Row(QString("Level %1").arg(j + 1), true, i, j));
+	// }
+	//
+	// QString name = nameTemplate.arg(i + 1);
 	// _rows.append(Row(name, true, i));
 
-	// endResetModel();
+
+	// Options *newRow = static_cast<Options *>(_boundTo->rowTemplate()->clone());
+	// OptionString *factorName = static_cast<OptionString *>(newRow->get("name"));
+	// factorName->setValue(fq("NewHypothesis"));
+
+	// OptionVariables *option = static_cast<OptionVariables *>(newRow->get("levels"));
+	// vector<string> levels = option->variables();
+
+	// _groups.push_back(newRow);
+
+	// FIXME: Below sentence should be used
+	_boundTo->setValue(_groups);
 }
